f1ef94623f558e52772265cec7150b0ed4ffa42b
Scott Barr <scottbarr@gmail.com>
2012-06-19 14:47:49 +0100

    Added a very basic QUnit test.

--- test/sliderbarr_test.js
+++ test/sliderbarr_test.js
@@ -1,6 +1,6 @@
 (function($) {
 
-  /*
+    /*
     ======== A Handy Little QUnit Reference ========
     http://docs.jquery.com/QUnit
 
@@ -17,17 +17,31 @@
       strictEqual(actual, expected, [message])
       notStrictEqual(actual, expected, [message])
       raises(block, [expected], [message])
-  */
+    */
 
-  module('sliderbarr', {
-    setup: function() {
-      
-    }
-  });
+    var slider = null;
 
-  test('is 1', 1, function() {
-    // Not a bad test to run on collection methods.
-    strictEqual(1, 1);
-  });
+    module('sliderbarr', {
+        setup: function() {
+                    slider = new SliderBarr({
+            'el'    : $('.slider'),
+            'value' : 50,
+            'step'  : 1
+        });
+        }
+    });
+
+    test('constructor settings', function() {
+        // Not a bad test to run on collection methods.
+        notStrictEqual(slider._settings.el, $('.slider'));
+        strictEqual(slider._settings.max, 100);
+        strictEqual(slider._settings.min, 0);
+        strictEqual(slider._settings.value, 50);
+        strictEqual(slider._settings.bar, true);
+        strictEqual(slider._settings.step, 1);
+        strictEqual(slider._settings.onChange, null);
+        strictEqual(slider._settings.onDrag, null);
+        strictEqual(slider._activeDrag, false);
+    });
 
 }(jQuery));
