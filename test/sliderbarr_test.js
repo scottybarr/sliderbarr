aa238d34fccdee422ef6d0046b441df791d4d4bd
Scott Barr <scottbarr@gmail.com>
2012-06-30 17:41:09 +0100

    Cleaning up and adding more tests.

--- test/sliderbarr_test.js
+++ test/sliderbarr_test.js
@@ -1,68 +1,161 @@
 (function($) {
 
-    /*
+  /*
     ======== A Handy Little QUnit Reference ========
     http://docs.jquery.com/QUnit
 
     Test methods:
-      expect(numAssertions)
-      stop(increment)
-      start(decrement)
+    expect(numAssertions)
+    stop(increment)
+    start(decrement)
     Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      raises(block, [expected], [message])
-    */
+    ok(value, [message])
+    equal(actual, expected, [message])
+    notEqual(actual, expected, [message])
+    deepEqual(actual, expected, [message])
+    notDeepEqual(actual, expected, [message])
+    strictEqual(actual, expected, [message])
+    notStrictEqual(actual, expected, [message])
+    raises(block, [expected], [message])
+  */
 
-    var slider = null;
+  var slider = null;
 
-    module('sliderbarr', {
-        setup: function() {
-            slider = new SliderBarr({
-                'el'    : $('.slider'),
-                'value' : 50,
-                'step'  : 1
-            });
-        }
+  module('sliderbarr', {
+    setup: function() {
+      slider = new SliderBarr({
+        'el'    : $('.slider'),
+        'value' : 50,
+        'step'  : 1
+      });
+    }
+  });
+
+  test('constructor settings', function() {
+    var renderStub        = sinon.stub(SliderBarr.prototype, '_render'),
+        renderEdgeStub    = sinon.stub(SliderBarr.prototype, '_renderEdgeLabels'),
+        initSelectorsStub = sinon.stub(SliderBarr.prototype, '_initSelectors'),
+        initEventsStub    = sinon.stub(SliderBarr.prototype, '_initEvents'),
+        renderHandleStub  = sinon.stub(SliderBarr.prototype, '_renderHandleChanges');
+
+
+    var slider = new SliderBarr({
+      'el'    : $('.slider'),
+      'value' : 50,
+      'step'  : 1
     });
 
-    test('constructor settings', function() {
-        var renderStub        = sinon.stub(SliderBarr.prototype, '_render'),
-            renderEdgeStub    = sinon.stub(SliderBarr.prototype, '_renderEdgeLabels'),
-            initSelectorsStub = sinon.stub(SliderBarr.prototype, '_initSelectors'),
-            initEventsStub    = sinon.stub(SliderBarr.prototype, '_initEvents'),
-            renderHandleStub  = sinon.stub(SliderBarr.prototype, '_renderHandleChanges');
+    strictEqual(slider._settings.el[0], $('.slider')[0]);
+    strictEqual(slider._settings.max, 100);
+    strictEqual(slider._settings.min, 0);
+    strictEqual(slider._settings.value, 50);
+    strictEqual(slider._settings.bar, true);
+    strictEqual(slider._settings.labels, false);
+    strictEqual(slider._settings.step, 1);
+    strictEqual(slider._settings.onChange, null);
+    strictEqual(slider._settings.onDrag, null);
+    strictEqual(slider._activeDrag, false);
+    deepEqual(slider._cache, []);
+    deepEqual(slider._sliderAttr, {});
 
+    ok(renderStub.called);
+    ok(!renderEdgeStub.called);
+    ok(initSelectorsStub.called);
+    ok(initEventsStub.called);
+    ok(renderHandleStub.called);
+  });
 
-        var slider = new SliderBarr({
-          'el'    : $('.slider'),
-          'value' : 50,
-          'step'  : 1
-        });
+  test('validate handles', function() {
+    slider.setValue(68);
 
-        notStrictEqual(slider._settings.el, $('.slider'));
-        strictEqual(slider._settings.max, 100);
-        strictEqual(slider._settings.min, 0);
-        strictEqual(slider._settings.value, 50);
-        strictEqual(slider._settings.bar, true);
-        strictEqual(slider._settings.labels, false);
-        strictEqual(slider._settings.step, 1);
-        strictEqual(slider._settings.onChange, null);
-        strictEqual(slider._settings.onDrag, null);
-        strictEqual(slider._activeDrag, false);
-        deepEqual(slider._cache, []);
-        deepEqual(slider._sliderAttr, {});
+    strictEqual(slider.getValue(), 68);
 
-        ok(renderStub.called);
-        ok(!renderEdgeStub.called);
-        ok(initSelectorsStub.called);
-        ok(initEventsStub.called);
-        ok(renderHandleStub.called);
+    sliderTwo = new SliderBarr({
+        'el'    : $('.slider'),
+        'value' : 23,
+        'step'  : 10
+      });
+
+    strictEqual(sliderTwo.getValue(), 20);
+    sliderTwo.setValue(178);
+    strictEqual(sliderTwo.getValue(), 100);
+    sliderTwo.setValue(-150);
+    strictEqual(sliderTwo.getValue(), 0);
+  });
+
+  test('render', function() {
+    expect(0);
+  });
+
+  test('render edge labels', function() {
+    expect(0);
+  });
+
+  test('initialise jquery selectors', function() {
+    expect(0);
+  });
+
+  test('initialise events', function() {
+    expect(0);
+  });
+
+  test('change handle', function() {
+    strictEqual(slider.getValue(), 50);
+
+    var dir = 'r';
+    slider._changeHandle(dir);
+
+    strictEqual(slider.getValue(), 51);
+
+    dir = 'l';
+    slider._changeHandle(dir);
+    strictEqual(slider.getValue(), 50);
+    slider._changeHandle(dir);
+    strictEqual(slider.getValue(), 49);
+  });
+
+  test('can handle on keydown', function() {
+    var myDir = null;
+
+    var changeStub = sinon.stub(SliderBarr.prototype, '_changeHandle', function(val) {
+      myDir = val;
     });
+
+    var e = {};
+
+    e['keyCode'] = 37;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'l');
+
+    e['keyCode'] = 40;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'l');
+
+    e['keyCode'] = 65;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'l');
+
+    e['keyCode'] = 83;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'l');
+
+    e['keyCode'] = 38;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'r');
+
+    e['keyCode'] = 39;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'r');
+    
+    e['keyCode'] = 68;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'r');
+
+    e['keyCode'] = 87;
+    slider._onHandleKeydown(e);
+    strictEqual(myDir, 'r');
+
+
+  });
 
 }(jQuery));
