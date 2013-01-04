12c75a6b690881d0ec54e7f6a91e52f833636187
Scott Barr <scottbarr@gmail.com>
2013-01-04 17:01:51 +0000

    Added stub tests

--- test/spec/sliderbarr.spec.js
+++ test/spec/sliderbarr.spec.js
@@ -1,13 +1,84 @@
 (function() {
 
     describe('Sliderbarr', function() {
+        var slider = null;
+
         beforeEach(function() {
+            slider = new SliderBarr({
+                el: $('#slider'),
+                value: 50,
+                step: 1
+            });
+        });
+
+        it ('can construct', function() {
 
         });
 
-        it ('can add', function() {
-            expect(1).toBe(1);
+        it ('can validate handles', function() {
+
         });
+
+        it ('can render', function() {
+
+        });
+
+        it ('can render edge labels', function() {
+
+        });
+
+        it ('can initialise jquery selectors', function() {
+
+        });
+
+        it ('can initialise events', function() {
+
+        });
+
+        it ('can change handle', function() {
+
+        });
+
+        it ('can handle on keydown', function() {
+
+        });
+
+        it ('can handle on mousedown', function() {
+
+        });
+
+        it ('can handle on mouse move', function() {
+
+        });
+
+        it ('can react to slider click', function() {
+
+        });
+
+        it ('can handle mouseup', function() {
+
+        });
+
+        it ('can fire on change', function() {
+
+        });
+
+        it ('can get value from mouse event', function() {
+
+        });
+
+        it ('can set slider value on drag', function() {
+
+        });
+
+        it ('can get value', function() {
+
+        });
+
+        it ('can set value', function() {
+
+        });
+
     });
 
 })();
