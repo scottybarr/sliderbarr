7a28bf8b8136089c7b1aa70fe2e02cbebcbcc41a
Scott Barr <scottbarr@gmail.com>
2013-01-04 18:14:44 +0000

    Added constructor tests

--- test/spec/sliderbarr.spec.js
+++ test/spec/sliderbarr.spec.js
@@ -12,70 +12,111 @@
         });
 
         it ('can construct', function() {
+            var fakeValidateHandles = spyOn(SliderBarr.prototype, '_validateHandles').andCallFake(function() {});
+            var fakeRender = spyOn(SliderBarr.prototype, '_render').andCallFake(function() {});
+            var fakeRenderLabels = spyOn(SliderBarr.prototype, '_renderEdgeLabels').andCallFake(function() {});
+            var fakeInitSelectors = spyOn(SliderBarr.prototype, '_initSelectors').andCallFake(function() {});
+            var fakeInitEvents = spyOn(SliderBarr.prototype, '_initEvents').andCallFake(function() {});
+            var fakeRenderHandleChanges = spyOn(SliderBarr.prototype, '_renderHandleChanges').andCallFake(function() {});
+
+            var slide = new SliderBarr({el: $('#slider')});
+
+            expect(slide._settings.el).toEqual($('#slider'));
+            expect(slide._settings.max).toBe(100);
+            expect(slide._settings.min).toBe(0);
+            expect(slide._settings.value).toBe(25);
+            expect(slide._settings.bar).toBeTruthy();
+            expect(slide._settings.labels).toBeFalsy();
+            expect(slide._settings.step).toBe(1);
+            expect(slide._settings.onChange).toBeNull();
+            expect(slide._settings.onDrag).toBeNull();
+
+            expect(fakeValidateHandles).toHaveBeenCalled();
+            expect(fakeRender).toHaveBeenCalled();
+            expect(fakeRenderLabels).not.toHaveBeenCalled();
+            expect(fakeInitSelectors).toHaveBeenCalled();
+            expect(fakeInitEvents).toHaveBeenCalled();
+            expect(fakeRenderHandleChanges).toHaveBeenCalled();
 
         });
 
-        it ('can validate handles', function() {
+        describe('Rendering', function() {
+
+            it ('can render', function() {
+
+            });
+
+            it ('can render edge labels', function() {
+
+            });
 
         });
 
-        it ('can render', function() {
+        describe('Selectors', function() {
+
+            it ('can initialise jquery selectors', function() {
+
+            });
 
         });
 
-        it ('can render edge labels', function() {
+        describe('Events', function() {
+
+            it ('can initialise events', function() {
+
+            });
+
+            it ('can change handle', function() {
+
+            });
+
+            it ('can handle on keydown', function() {
+
+            });
+
+            it ('can handle on mousedown', function() {
+
+            });
+
+            it ('can handle on mouse move', function() {
+
+            });
+
+            it ('can react to slider click', function() {
+
+            });
+
+            it ('can handle mouseup', function() {
+
+            });
+
+            it ('can fire on change', function() {
+
+            });
 
         });
 
-        it ('can initialise jquery selectors', function() {
+        describe('Set and Get Values', function() {
 
-        });
+            it ('can validate handles', function() {
 
-        it ('can initialise events', function() {
+            });
 
-        });
+            it ('can get value from mouse event', function() {
 
-        it ('can change handle', function() {
+            });
 
-        });
+            it ('can set slider value on drag', function() {
 
-        it ('can handle on keydown', function() {
+            });
 
-        });
+            it ('can get value', function() {
 
-        it ('can handle on mousedown', function() {
+            });
 
-        });
+            it ('can set value', function() {
 
-        it ('can handle on mouse move', function() {
-
-        });
-
-        it ('can react to slider click', function() {
-
-        });
-
-        it ('can handle mouseup', function() {
-
-        });
-
-        it ('can fire on change', function() {
-
-        });
-
-        it ('can get value from mouse event', function() {
-
-        });
-
-        it ('can set slider value on drag', function() {
-
-        });
-
-        it ('can get value', function() {
-
-        });
-
-        it ('can set value', function() {
+            });
 
         });
 
